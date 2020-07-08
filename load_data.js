0e3a4247282b5ac6e1e978da80eb81dc102623d5
Cong <rampadc@users.noreply.github.com>
2020-07-08 13:39:09 +1000

    Inserted ppl data

--- load_data.js
+++ load_data.js
@@ -1,11 +1,22 @@
+/******************************************************************************
+ * Dependencies
+ *****************************************************************************/
 require('dotenv').config();
 
 const GraknClient = require('grakn-client');
 const { createLogger, format, transports } = require('winston');
+const parse = require('csv-parse/lib/sync');
+const fs = require('fs');
 
-/**
+/******************************************************************************
+ * External variables
+ *****************************************************************************/
+const CLIENT_URI = `${process.env.HOST}:${process.env.PORT}`;
+const KEYSPACE = process.env.KEYSPACE;
+
+/******************************************************************************
  * Create logger
- */
+ *****************************************************************************/
 const logger = createLogger({
   level: 'info',
   format: format.combine(
@@ -22,14 +33,58 @@ const logger = createLogger({
   ]
 });
 
-/**
+/******************************************************************************
+ * Data loading functions
+ *****************************************************************************/
+async function insertPersons(transaction) {
+  logger.info(`Inserting persons' data`);
+
+  const data = fs.readFileSync('./data/person.csv', 'utf-8');
+  const records = parse(data, {delimiter: ',', columns: true});
+  /*
+      Example of a record:
+      {
+        'first-name': 'Catalina',
+        'last-name': 'Sargent',
+        gender: 'female',
+        'phone-number': '621-620-0394',
+        city: 'Frankfurt',
+        email: 'catalinasargent@googlemail.com'
+      }
+    */
+  for (let record of records) {
+    const query = `insert $person isa person
+                , has email "${record['email']}"
+                , has first-name "${record['first-name']}"
+                , has last-name "${record['last-name']}"
+                , has city "${record['city']}"
+                , has phone-number "${record['phone-number']}"
+                , has gender "${record['gender']}"
+                ;
+    `;
+    logger.debug(`Query: ${query}`);
+    await transaction.query(query);
+  }
+
+  logger.debug(`Committing changes to person data`);
+  await transaction.commit();
+
+  logger.info('Completed inserting person data');
+}
+
+/******************************************************************************
  * Main code
- */
+ *****************************************************************************/
 (async() => {
-  const client = new GraknClient(`${process.env.HOST}:${process.env.PORT}`);
-  const session = await client.session(process.env.KEYSPACE);
+  const client = new GraknClient(CLIENT_URI);
+  const session = await client.session(KEYSPACE);
   
-  logger.info(`Connected to grakn server at ${process.env.HOST}:${process.env.PORT} using keyspace ${process.env.KEYSPACE}`);
+  logger.info(`Connected to grakn server at ${CLIENT_URI} using keyspace ${KEYSPACE}`);
+
+  logger.debug(`Opening a write transaction to perform a write query...`);
+  const transaction = await session.transaction().write();
+
+  await insertPersons(transaction);
 
   logger.info(`Closing session...`);
   await session.close();
